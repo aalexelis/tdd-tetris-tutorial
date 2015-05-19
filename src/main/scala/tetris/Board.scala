f8c65c92e010aff74eed123a7f06dd1d120e090f
andreas@bizreach.co.jp <andreas@bizreach.co.jp>
2015-05-19 13:59:27 +0900

    using a mutable map, down to it_is_still_falling_on_the_last_row

--- src/main/scala/tetris/Board.scala
+++ src/main/scala/tetris/Board.scala
@@ -6,4 +6,30 @@ package tetris
 
 class Board(val rows: Int, val columns: Int) {
 
+  val boardState = scala.collection.mutable.Map[(Int, Int),Block]()
+  override def toString() = (1 to rows).map(y => {
+    (1 to columns).map(x => {
+      boardState.get((y,x)).map(_.render).getOrElse(".")
+    }).mkString("","","\n")
+  }).reverse.mkString("")
+  
+  var hasFallingFlg = false
+  def hasFalling() = hasFallingFlg
+  def drop(block: Block) = {
+    if (hasFallingFlg) throw new IllegalStateException("already falling")
+    hasFallingFlg = true
+    boardState.put((rows, columns/2+1),block) //TODO
+  }
+  
+  def tick() = {
+    boardState.clone.foreach{case ((y,x), b) =>
+      boardState.put((y-1,x),b)
+      boardState.remove((y,x))
+    }
+  }
+
+}
+
+class Block(kind: Char){
+  def render:String = kind.toString
 }
